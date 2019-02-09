fce739cd17586702d937dc1c273a97c15aca8cd8
Kim Heidorn <heidorkn@gmail.com>
2019-02-09 16:59:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,13 @@
 # Write your code here.
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
   list = []
-  array.each do 
+  array.each do |name|
+    list.push("Hello, my name is #{name}.")
+  end
+  return list
+end
